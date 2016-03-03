dd3a4b16e1262447818552e3b900f3af3cee36c2
Sylvain CAUSSINUS <scaussin@e1r10p1.42.fr>
2016-03-03 16:06:08 +0100

    rm

--- bircd/main.c
+++ bircd/main.c
@@ -1,4 +1,8 @@
-
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include "bircd.h"
 
 int	main(int ac, char **av)
@@ -10,4 +14,47 @@ int	main(int ac, char **av)
 	srv_create(&e, e.port);
 	main_loop(&e);
 	return (0);
+}
+
+void		init_env(t_env *e)
+{
+	int		i;
+	struct rlimit	rlp;
+
+	X(-1, getrlimit(RLIMIT_NOFILE, &rlp), "getrlimit");
+	e->maxfd = rlp.rlim_cur;
+	e->fds = (t_fd*)Xv(NULL, malloc(sizeof(*e->fds) * e->maxfd), "malloc");
+	i = 0;
+	while (i < e->maxfd)
+	{
+		clean_fd(&e->fds[i]);
+		i++;
+	}
+}
+
+void	get_opt(t_env *e, int ac, char **av)
+{
+	if (ac != 2)
+	{
+		fprintf(stderr, USAGE, av[0]);
+		exit(1);
+	}
+	e->port = atoi(av[1]);
+}
+
+void	srv_create(t_env *e, int port)
+{
+	int					s;
+	struct sockaddr_in	sin;
+	struct protoent		*pe;
+	
+	pe = (struct protoent*)Xv(NULL, getprotobyname("tcp"), "getprotobyname");
+	s = X(-1, socket(PF_INET, SOCK_STREAM, pe->p_proto), "socket");
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(port);
+	X(-1, bind(s, (struct sockaddr*)&sin, sizeof(sin)), "bind");
+	X(-1, listen(s, 42), "listen");
+	e->fds[s].type = FD_SERV;
+	e->fds[s].fct_read = srv_accept;
 }
